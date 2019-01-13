6ad9a349bdf2e839a417194eace78a1b500b6b52
Mourad Samy <mouradsamyroushdy@gmail.com>
2019-01-13 13:29:08 +0200

    Fix album-list.component.spec by adding missing dependencies.

--- src/app/components/albums/album-list/album-list.component.spec.ts
+++ src/app/components/albums/album-list/album-list.component.spec.ts
@@ -1,19 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AlbumListComponent } from './album-list.component';
-import { AlbumsService } from 'src/app/services';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-xdescribe('AlbumsComponent', () => {
+import { AlbumListComponent } from './album-list.component';
+import { AlbumsService, AppConfig } from 'src/app/services';
+import { AppConfigMock } from 'src/app/mocks';
+
+describe('AlbumsComponent', () => {
   let component: AlbumListComponent;
   let fixture: ComponentFixture<AlbumListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AlbumListComponent],
-      imports: [RouterTestingModule],
-      providers: [AlbumsService],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        { provide: AppConfig, useClass: AppConfigMock },
+        AlbumsService
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
